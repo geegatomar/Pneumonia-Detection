eb857521f12d4eca9553c6db7462edcd27cde67e
geegatomar <geegatomar@gmail.com>
2021-01-10 17:53:50 +0530

    modifying build_dataset

--- build_dataset.py
+++ build_dataset.py
@@ -1,5 +1,12 @@
-import config.lung_config as config
+from config import lung_config as config
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import cv2
+img_height = config.IMG_HEIGHT
+img_width = config.IMG_WIDTH
+batch_size = config.BATCH_SIZE
 
 # Create Image Data Generator for Train Set
 image_gen = ImageDataGenerator(
@@ -8,5 +15,48 @@ image_gen = ImageDataGenerator(
     zoom_range=0.2,
     horizontal_flip=True,
 )
+
 # Create Image Data Generator for Test/Validation Set
 test_data_gen = ImageDataGenerator(rescale=1./255)
+
+train = image_gen.flow_from_directory(
+    config.TRAIN_PATH,
+    target_size=(img_height, img_width),
+    color_mode='grayscale',
+    class_mode='binary',
+    batch_size=batch_size
+)
+
+test = test_data_gen.flow_from_directory(
+    config.TEST_PATH,
+    target_size=(img_height, img_width),
+    color_mode='grayscale',
+    shuffle=False,
+    # setting shuffle as False just so we can later compare it with predicted values without having indexing problem
+    class_mode='binary',
+    batch_size=batch_size
+)
+
+valid = test_data_gen.flow_from_directory(
+    config.VAL_PATH,
+    target_size=(img_height, img_width),
+    color_mode='grayscale',
+    class_mode='binary',
+    batch_size=batch_size
+)
+
+
+print(train)
+
+plt.figure(figsize=(12, 12))
+for i in range(0, 10):
+    plt.subplot(2, 5, i+1)
+    for X_batch, Y_batch in train:
+        image = X_batch[0]
+        dic = {0: 'NORMAL', 1: 'PNEUMONIA'}
+        plt.title(dic.get(Y_batch[0]))
+        plt.axis('off')
+        plt.imshow(np.squeeze(image), cmap='gray', interpolation='nearest')
+        break
+plt.tight_layout()
+plt.show()
