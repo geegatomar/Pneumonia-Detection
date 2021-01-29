22986d41723a8c9f7a1a38e8f99398ac78797b09
geegatomar <geegatomar@gmail.com>
2021-01-29 19:38:18 +0530

    webapp

--- webapp.py
+++ webapp.py
@@ -22,3 +22,42 @@ st.write("""
 # X-Ray Classification [Pneumonia / Normal]
 by Shivangi 
 """)
+
+
+temp = st.file_uploader("Upload X-Ray Image")
+#temp = temp.decode()
+
+buffer = temp
+temp_file = NamedTemporaryFile(delete=False)
+if buffer:
+    temp_file.write(buffer.getvalue())
+    st.write(image.load_img(temp_file.name))
+
+
+if buffer is None:
+    st.text("Oops! that doesn't look like an image. Try again.")
+
+else:
+
+    shiv_img = image.load_img(temp_file.name, target_size=(
+        500, 500), color_mode='grayscale')
+
+    # Preprocessing the image
+    pp_shiv_img = image.img_to_array(shiv_img)
+    pp_shiv_img = pp_shiv_img/255
+    pp_shiv_img = np.expand_dims(pp_shiv_img, axis=0)
+
+    # predict
+    shiv_preds = cnn.predict(pp_shiv_img)
+    if shiv_preds >= 0.5:
+        out = ('I am {:.2%} percent confirmed that this is a Pneumonia case'.format(
+            shiv_preds[0][0]))
+
+    else:
+        out = ('I am {:.2%} percent confirmed that this is a Normal case'.format(
+            1-shiv_preds[0][0]))
+
+    st.success(out)
+
+    image = Image.open(temp)
+    st.image(image, use_column_width=True)
